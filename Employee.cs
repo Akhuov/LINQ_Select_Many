fb2cb263da490f21d05b12863eef5259f81399dc
user <user@WIN-F7NIMF7A3VO>
2023-11-21 07:22:25 +0500

    Добавьте файлы проекта.

---
+++ Employee.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_002
+{
+        public class Employee
+        {
+            public int Id { get; set; }
+
+            public string? FirstName { get; set; }
+
+            public string? LastName { get; set; }
+
+            public int Age { get; set; }
+
+            public float Salary { get; set; }
+
+            public List<string> ProgrammingLanguages { get; set; }
+
+            public List<string> CreditCards { get; set; }
+
+            public List<string> Emails { get; set; }
+
+            public static List<Employee> GetEmployees()
+            {
+                return new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                FirstName = "Salohiddin",
+                LastName = "Sunnatov",
+                Age = 19,
+                Salary = 1000,
+                ProgrammingLanguages = new List<string>
+                {
+                    "C#",
+                    "C",
+                    "C++"
+                },
+                CreditCards = new List<string>
+                {
+                    "123123123",
+                    "563643534",
+                },
+                Emails = new List<string>
+                {
+                    "iddiunna@gmail.com",
+                    "ankabut571@gmail.com",
+                    "SalohiddinSunnatov@gmail.com"
+                }
+            },
+
+            new Employee
+            {
+                Id = 2,
+                FirstName = "Bahrom",
+                LastName = "Bahromov",
+                Age = 18,
+                Salary = 1200,
+                ProgrammingLanguages = new List<string>
+                {
+                    "C#",
+                    "C",
+                    "C++"
+                },
+                CreditCards = new List<string>
+                {
+                    "47352698743564",
+                    "2495640985",
+                    "4837640574232"
+                },
+                Emails = new List<string>
+                {
+                    "BahromBahromov@gmail.com",
+                }
+            },
+
+            new Employee
+            {
+                Id = 3,
+                FirstName = "Islom",
+                LastName = "Karimov",
+                Age = 20,
+                Salary = 434,
+                ProgrammingLanguages = new List<string>
+                {
+                    "C#",
+                    "C",
+                    "C++"
+                },
+                CreditCards = new List<string>
+                {
+                    "21123131233",
+                },
+                Emails = new List<string>
+                {
+                    "IslomKarimov@gmail.com",
+                }
+            },
+            new Employee
+            {
+                Id = 4,
+                FirstName = "Akrom",
+                LastName = "Fozilov",
+                Age = 54,
+                Salary = 777,
+                ProgrammingLanguages = new List<string>
+                {
+                    "Python",
+                    "Javascript",
+                    "Java"
+                },
+                CreditCards = new List<string>
+                {
+                    "4385603894",
+                },
+                Emails = new List<string>
+                {
+                    "AkromFozilov@gmail.com",
+                    "FozilovAkrom@gmail.com",
+                }
+            },
+            new Employee
+            {
+                Id = 5,
+                FirstName = "Vali",
+                LastName = "Aliyev",
+                Age = 24,
+                Salary = 455,
+                ProgrammingLanguages = new List<string>
+                {
+                    "Paskal",
+                    "Rust",
+                    "Ruby"
+                },
+                CreditCards = new List<string>
+                {
+                    "1231234132",
+                    "8999676454",
+                    "3242343243"
+                },
+                Emails = new List<string>
+                {
+                    "ValiAliyev@gmail.com",
+                    "AliyevVali@gmail.com"
+                }
+            }
+        };
+            }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using LINQ_002;
+
+namespace DemoConsoleApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // HomeTask
+        // With Select
+        Console.WriteLine("\n   With Select \n");
+
+        var employees = Employee.GetEmployees().Select(x => new
+        {
+            Id = x.Id,
+            FirstName = x.FirstName,
+            LastName = x.LastName,
+            Age = x.Age,
+            Salary = x.Salary,
+            Languages = string.Join(", ", x.ProgrammingLanguages),
+            CreditCards = string.Join(", ", x.CreditCards),
+            Emails = string.Join(", ", x.Emails)
+        });
+
+        foreach (var emp in employees)
+        {
+            Console.WriteLine("FirstName: {0}", emp.FirstName);
+            Console.WriteLine("Programming Languages: {0}", emp.Languages);
+            Console.WriteLine("Emails: {0}", emp.Emails);
+            Console.WriteLine("Credit Cards: {0}", emp.CreditCards);
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\n   With Select Many \n");
+
+        // With Select Many
+        var emps = Employee.GetEmployees()
+                .SelectMany(x => x.ProgrammingLanguages, (emp, lang) => new
+                {
+                    emp.Id,
+                    emp.FirstName,
+                    emp.LastName,
+                    emp.Age,
+                    emp.Salary,
+                    Language = lang,
+                    CreditCards = emp.CreditCards,
+                    Emails = emp.Emails
+                })
+                .SelectMany(x => x.CreditCards, (emp, card) => new
+                {
+                    emp.Id,
+                    emp.FirstName,
+                    emp.LastName,
+                    emp.Age,
+                    emp.Salary,
+                    emp.Language,
+                    CreditCard = card,
+                    emp.Emails
+                })
+                .SelectMany(x => x.Emails, (emp, email) => new
+                {
+                    emp.Id,
+                    emp.FirstName,
+                    emp.LastName,
+                    emp.Age,
+                    emp.Salary,
+                    emp.Language,
+                    emp.CreditCard,
+                    Email = email
+                });
+
+        foreach (var emp in emps)
+        {
+            Console.WriteLine("FirstName: {0}", emp.FirstName);
+            Console.WriteLine("Programming Language: {0}", emp.Language);
+            Console.WriteLine("Email: {0}", emp.Email);
+            Console.WriteLine("Credit Card: {0}", emp.CreditCard);
+            Console.WriteLine();
+        }
+
+         var query2 = Employee.GetEmployees().Select(x => new
+                {
+                    EmployeeName = string.Join("", x.FirstName),
+                    Programm = string.Join(",", x.ProgrammingLanguages),
+                    Email = string.Join(",", x.Emails)
+                }).ToList();
+
+
+            foreach ( var employee in query2 )
+            {
+                Console.WriteLine(employee.EmployeeName + " => " + employee.Programm + " => " + employee.Email);
+            }
+
+
+
+    }
+}
